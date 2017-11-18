40b1a9ff900ebd6cebe1e23449f01a10d9e17b37
harenarium <carolinel2610@gmail.com>
2017-11-18 18:28:14 +0000

    automatically backed up by learn

---
+++ models/Question.js
@@ -0,0 +1,38 @@
+const db = require("../config/db")
+
+class Question{
+  static CreateTable() {
+    return new Promise(function(resolve){
+      const sql = `CREATE TABLE questions (
+        id INTEGER PRIMARY KEY,
+        content TEXT
+      )`
+
+      db.run(sql, function(){
+        resolve("questions table created")
+      })
+    })
+  }
+
+  constructor(content){
+    this.content = content
+  }
+
+  insert(){
+
+     const sql = 'INSERT INTO questions (content) VALUES ?' //check this
+     const self = this
+
+    return new Promise(function(resolve){ //promise(function(resolve){}) two sets of ()
+       db.run(sql,[self.content],function(){ //don't need interpolation around self.content
+         self.id = this.lastID  // this.lastID not this.id
+         resolve(self)
+       })
+    })
+
+
+  }
+
+}
+
+module.exports = Question;
